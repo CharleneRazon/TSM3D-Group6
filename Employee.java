129b2b59e4b4404b80fbe0475b49bb9ccf35dc4d
nerisaflor <nr.flor0501@student.tsu.edu.ph>
2021-03-19 23:27:43 +0800

    NRF

--- Employee.java
+++ Employee.java
@@ -37,3 +37,32 @@ class E_Form {
                               this.salary = salary;  
 
                }
+	public String getName() {  
+
+                              return name;  
+
+               }  
+
+               public void setName(String name) {  
+
+                              this.name = name;  
+
+               }  
+
+               public String getAddress() {  
+
+                              return address;  
+
+               }  
+
+               public void setAddress(String address) {  
+
+                              this.address = address;  
+
+               }  
+
+               public String getDepartment() {  
+
+                              return department;  
+
+               }
